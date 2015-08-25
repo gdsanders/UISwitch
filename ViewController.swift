20e5170c73296ee961a93bf525fd6cc9d75419f4
gdsanders <gd@digitalequity.com>
2015-08-25 16:57:37 -0500

    Connections and IBOutlets

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var demoSwitch: UISwitch!
+    @IBOutlet weak var switchStateLabel: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +26,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func changeStateTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func switchStateTapped(sender: UISwitch) {
+        
+        
     }
 
 
